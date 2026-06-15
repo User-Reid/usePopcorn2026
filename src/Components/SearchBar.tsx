8db66228344f2b534ae69937880acc5c6762c7b9
Reid Herrera <reidherrerabusiness@gmail.com>
2026-06-15 16:52:42 -0500

    added dom selection for mount focus :)

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import type { SearchBarProps } from "../Types/Types";
 
 export default function SearchBar({ query, setQuery }: SearchBarProps) {
+  useEffect(() => {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  }, []);
+
   return (
     <input
       className="search"
